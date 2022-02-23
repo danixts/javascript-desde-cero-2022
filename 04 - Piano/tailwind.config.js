be61cfdf98d2744d4d6df8662fdf4a05f71d246c
Daniel <dante789ki@gmail.com>
2022-02-23 16:42:29 -0400

    [C10] template piano

---
+++ 04 - Piano/tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./index.html", "./main.js", "./src/**/*.js"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      keyframes: {
+        zoom: {
+          from: { transform: "scale(0)", opacity: 0 },
+          to: { transform: "scale(1)", opacity: 1 },
+        },
+      },
+      animation: {
+        zoom: "animation: zoom .3s linear;",
+      },
+      colors: {
+        primary: colors.orange,
+      },
+    },
+  },
+  plugins: [],
+};
